efce5436faf4537668a2c8edcb9d6cc9578758c0
Dhwani <you@example.com>
2022-02-25 11:22:25 +0530

    core templates

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from .views import UserLogin,Logout
+
+urlpatterns = [
+   
+   path('login/',UserLogin.as_view(), name="login"),
+   path('logout/',Logout.as_view(), name="logout"),
+
+]
---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView,LogoutView
+# Create your views here.
+class UserLogin(LoginView):
+    template_name = 'core/login.html'
+
+class Logout(LogoutView):
+    pass
