739aa997125f1d96fc973e5d0376d3a0368d1972
Dhwani <you@example.com>
2022-02-27 20:39:58 +0530

    login and logout

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
    
    path('login/',UserLogin.as_view(), name="login"),
    path('logout/',Logout.as_view(), name="logout"),
-
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView,LogoutView
-# Create your views here.
+
+# Create your views here..
 class UserLogin(LoginView):
     template_name = 'core/login.html'
 
